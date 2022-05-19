d72574d81e4313091e33d7d0c7f7a4a04fb792cb
Enzo-taiel <57237206+Enzo-taiel@users.noreply.github.com>
2022-05-19 19:44:09 -0300

    Delete index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Listen Server
-const main_1 = require("./main");
-require('dotenv').config();
-const srv = new main_1.Server();
-srv.middleware();
-srv.startServer();
