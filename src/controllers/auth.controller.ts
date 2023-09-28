157537c1ea43ec525ddc52382001cfb75193495a
Enzo <enzonavarro28@gmail.com>
2023-09-28 15:01:04 -0300

    first commit

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,3 +8,13 @@ export const SigninController = (req: Request, res: Response) => {
 
   return res.end("end")
 }
+
+export const SignupController = (req: Request, res: Response) => {
+  const { name, lastname, username, password } = req.body;
+  if (!name) return res.status(402).json({ error: true, message: "" })
+  if (!lastname) return res.status(402).json({ error: true, message: "" })
+  if (!username) return res.status(402).json({ error: true, message: "" })
+  if (!password) return res.status(402).json({ error: true, message: "" })
+
+  return res.end("end")
+}
