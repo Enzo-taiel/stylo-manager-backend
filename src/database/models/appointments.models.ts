e5687a95ed83579c58239f576cde49b1fc1ed4f5
Enzo-Taiel <enzonavarro28@gmail.com>
2024-12-02 13:38:46 -0300

    se eliminaron importaciones innecesarias

--- src/database/models/appointments.models.ts
+++ src/database/models/appointments.models.ts
@@ -1,7 +1,6 @@
 import { Schema } from 'mongoose';
 // SCHEMAS
 import { AppointmentsSchema } from '../schemas'
-import { ClientsModel, EmployeesModel } from './index.models';
 
 const Appointments_Schema = new Schema(AppointmentsSchema, { timestamps: true });
 
