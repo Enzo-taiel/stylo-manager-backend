0ecc9498559211b7cbe14be955e7466e7780f338
Enzo-Taiel <enzonavarro28@gmail.com>
2024-12-02 13:42:23 -0300

    se agregaron rutas

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 // CONTROLLERS
-import { CreateMessageContactController } from '../controllers/contact'
+import { CreateMessageContactController, getAllMessages, getAllMessageByPhone } from '../controllers/contact'
 import ContactMessageValidateFieldsMiddleware from '../middleware/contact/createMessageContactMiddleware'
 
 const routerContact = Router()
 
+routerContact.get("/all", getAllMessages)
+routerContact.get("/message/:phone", getAllMessageByPhone)
 routerContact.post("/message", ContactMessageValidateFieldsMiddleware, CreateMessageContactController)
 
 export default routerContact
--- src/routes/sales.routes.ts
+++ src/routes/sales.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 // CONTROLLERS
-import { CreateSalesController, ObtainSalesAllController } from '../controllers/sales'
+import { CreateSalesController, ObtainSalesAllController, DeleteSalesController } from '../controllers/sales'
 
 const routerSales = Router()
 
 routerSales.get("/all", ObtainSalesAllController)
 // routerSales.get("/:employeeId", CreatePaymentController)
 routerSales.post("/create", CreateSalesController)
+routerSales.delete("/delete/:saleId", DeleteSalesController)
 
 export default routerSales
