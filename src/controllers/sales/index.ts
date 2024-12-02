273658a80b7b31f3c1e4e3e741273cf5986b8319
Enzo-Taiel <enzonavarro28@gmail.com>
2024-12-02 13:39:10 -0300

    se agrego una exportacion para con controlador de delete sale

--- src/controllers/sales/index.ts
+++ src/controllers/sales/index.ts
@@ -1,2 +1,4 @@
 export { CreateSalesController } from "./create.controller"
 export { ObtainSalesAllController } from "./obtain.controller"
+export { DeleteSalesController } from "./delete.controller"
+
