1fc94b2734c142199aeaa3136fc28b13090cc3a7
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-05 15:11:43 -0300

    Refactor createServiceMiddleware: improve validation messages and correct field names

--- src/middleware/services/createServiceMiddleware.ts
+++ src/middleware/services/createServiceMiddleware.ts
@@ -1,4 +1,4 @@
-import { body, Meta } from 'express-validator'
+import { body } from 'express-validator'
 import mongoose from 'mongoose'
 import { ServicesModel } from '../../database/models/index.models'
 
@@ -7,15 +7,15 @@ const verifyThatServiceAlreadyExist = async (title: string) => {
   if (user) throw new Error('El servicio ya se encuentra establecido.');
 }
 
-const validationDescriptions = (skills: string[], { req }: Meta) => {
+const validationDescriptions = (skills: string[]) => {
   if (!skills.every(skill => typeof skill === 'string')) {
-    throw new Error("Todas las habilidades deben ser cadenas de texto.");
+    throw new Error("Todas las descripciones deben ser cadenas de texto.");
   }
   return true;
 }
 
-const validationEmployeesVailable = (employees_vailable: string[], { req }: Meta) => {
-  if(employees_vailable.length === 0) throw new Error('Ingresa al menos 1 id')
+const validationEmployeesVailable = (employees_vailable: string[]) => {
+  if (employees_vailable.length === 0) throw new Error('Ingresa al menos 1 id')
   employees_vailable.forEach(id => {
     if (!mongoose.Types.ObjectId.isValid(id)) {
       throw new Error('Cada elemento debe ser un ObjectId válido');
@@ -27,36 +27,38 @@ const validationEmployeesVailable = (employees_vailable: string[], { req }: Meta
 // Middleware de validación para el objeto de inicio de sesión
 const validateFieldsCreateService = [
   body('title')
-    .isString()
-    .withMessage("El valor ingresado debe ser una cadena de texto.")
     .notEmpty()
     .withMessage("El titulo del servicio es requerido.")
-    .isLength({ min: 5 })
-    .withMessage('El nombre de usuario debe tener al menos 5 caracteres')
+    .isString()
+    .withMessage("El titulo ingresado para el servicio debe ser un string.")
     .custom(verifyThatServiceAlreadyExist),
-  body('descriptions')
-    .isArray()
-    .withMessage("Las habilidades deben ser un array.")
+  body('subtitle')
     .notEmpty()
-    .withMessage("Debes ingresar al menos una habilidad.")
+    .withMessage("Debes ingresar un subtitulo para el servicio.")
+    .isString()
+    .withMessage("El subtitulo del servicio deben ser un string."),
+  body('descriptions')
+    .notEmpty()
+    .withMessage("Debes ingresar al menos una descripcion del servicio.")
+    .isArray()
+    .withMessage("Las descripciones del servicio deben estar en un array.")
     .custom(validationDescriptions),
   body('price')
-    .isString()
-    .withMessage("El precio ingresado debe ser una cadena de texto.")
     .notEmpty()
     .withMessage("El precio del servicio es requerido.")
+    .isString()
+    .withMessage("El precio ingresado para el servicio debe ser una cadena de texto.")
     .isLength({ min: 4 }),
   body('price_kids')
     .isString()
     .withMessage("El precio ingresado debe ser una cadena de texto.")
     .optional()
     .isLength({ min: 4 }),
-  body('employees_vailable')
-    .isArray()
+  body('employees_available')
     .notEmpty()
-    .withMessage("Debes ingresar un array con ids de trabajadores disponibles para este servicio.")
-    .custom(validationEmployeesVailable)
-    .withMessage('Cada elemento debe ser un ObjectId válido')
+    .withMessage("Debes ingresar al menos un empleado disponibles para este servicio.")
+    .isArray()
+    .withMessage("Los empleados del servicio deben estar en un array.")
 ];
 
 export default validateFieldsCreateService
