14e57364282daa3427fea997e0a5178ad78152d3
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-13 20:12:25 -0300

    Add appointment session retrieval route: include getAppointmentBySessionId in appointments routes and update create appointment route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 // CONTROLLERS
-import { getAllAppointments, getAppointementById} from '../controllers/appointments'
+import {
+  getAllAppointments, getAppointementById,
+  getAppointmentBySessionId, createAppointment
+} from '../controllers/appointments'
 // MIDDLEWARE
+import AppointmentValidateFieldsMiddleware from '../middleware/appointment/createAppointmentMiddleware'
 
 const routerAppointments = Router()
 routerAppointments.get("/all", getAllAppointments)
+routerAppointments.get("/session/all", getAppointmentBySessionId)
+routerAppointments.post("/create", createAppointment)
 routerAppointments.get("/:appointmentId", getAppointementById)
 
 export default routerAppointments
