4f6f3aeeeac111bc5f434fbb30d9da83d41d9421
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-14 17:31:21 -0300

    correction tipography

--- src/controllers/appointments/create.controller.ts
+++ src/controllers/appointments/create.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { AppointmentsModel, EmployeesModel } from '../../database/models/index.models';
 
-export const createAppointment = async (req: Request, res: Response) => {
+export const createAppointmentController = async (req: Request, res: Response) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array()[0] });
