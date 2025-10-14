c9bac1b8eb0955753c181f081ffa26fc012fec6f
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-14 17:32:18 -0300

    se agrego el controlador para eliminar una reserva.

---
+++ src/controllers/appointments/delete.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { AppointmentsModel, EmployeesModel, TempAppointmentsModel } from '../../database/models/index.models';
+import { deleteOneAppointment } from './delete';
+import mongoose from 'mongoose';
+
+export const deleteAppointmentByIdController = async (req: Request, res: Response) => {
+  const appointmentId = req.params.appointmentId
+  try {
+    const appointment = await AppointmentsModel.findById(appointmentId)
+      .populate({ path: "client", select: "phone full_name" })
+      .populate({ path: "service", select: "title" })
+      .populate({ path: "employee", select: "_id full_name expoPushToken" })
+
+    if (!appointment) return res.status(204).json({ message: "No existe tal reservacion.", success: false })
+    const sessionTransaction = await mongoose.startSession()
+    sessionTransaction.startTransaction()
+
+    await TempAppointmentsModel.create({
+      employeeName: appointment.employee.full_name,
+      clientName: appointment.client.full_name,
+      phoneClient: appointment.client.phone,
+      appointmentId: appointment._id,
+      expoPushToken: appointment.employee.expoPushToken,
+      date: appointment.date,
+      hour: appointment.hour
+    })
+
+    await EmployeesModel.findByIdAndUpdate(appointment.employee._id, { $pull: { appointments: appointment._id } }, { new: true })
+    await deleteOneAppointment(appointment._id)
+
+    await sessionTransaction.commitTransaction()
+
+    return res.status(200).json({ message: "Appointments delete successfully." })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: "Error internal Server." })
+  }
+
+  // try {
+
+
+  //   await appointment.deleteOne()
+  //   await EmployeesModel.findByIdAndUpdate(employeeId, { $pull: { appointments: reservationId } }, { new: true })
+
+  //   return { success: true }
+  // } catch (error) {
+  //   console.log(error)
+  //   return { error: true }
+  // }
+
+}
