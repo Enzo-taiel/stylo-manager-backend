b20562ec23bd64f7f5413a858c7cd8c595c24c74
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-17 01:48:30 -0300

    se creo la ruta para poder establecer el objeto subscription a la sesion del usuario para poder enviarle recordatorios

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express'
-// CONTROLLERS
-import { CreatePushNotificationController } from '../controllers/notifications'
+import { Router } from "express";
+import { CreatePushNotificationController, subscriptionCreateController } from "../controllers/notifications";
 
 const routerNotification = Router()
 
 routerNotification.post("/send", CreatePushNotificationController)
+routerNotification.put("/subscription/create", subscriptionCreateController)
 
 export default routerNotification
