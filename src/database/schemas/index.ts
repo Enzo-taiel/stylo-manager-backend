b38efc2766df4dcf734831817b968a78aabd50dd
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-17 01:46:52 -0300

    correcciones de errores tipograficos

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -1,10 +1,9 @@
 export { UserSchema } from "./user.schema"
-export { EmployeesSchema } from "./employee.schema"
-export { ServicesSchema } from "./services.schema"
+export { SalesSchema } from "./sales.schema"
 export { ClientsSchema } from "./clients.schema"
+export { ContactSchema } from "./contact.schema"
+export { SessionSchema } from "./sessions.schema"
+export { ServicesSchema } from "./services.schema"
+export { EmployeesSchema } from "./employee.schema"
 export { AppointmentsSchema } from "./appointment.schema"
 export { TempAppointmentsSchema } from "./temp.appointment.schema"
-
-export { ContactSchema } from "./contact.schema"
-export { SalesSchema } from "./sales.schema"
-
