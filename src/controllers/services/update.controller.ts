40d160a2f199e4d44f569e7618392640cd11263b
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-24 20:12:05 -0300

    feat: add UpdateServiceController and update routes for service management

---
+++ src/controllers/services/update.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+import { ServicesModel } from "../../database/models/index.model";
+
+export const UpdateServiceController = async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+  const { serviceId } = req.params;
+  const updateData = req.body;
+
+  try {
+    // Buscar al empleado por ID
+    const employee = await ServicesModel.findById(serviceId);
+    if (!employee) return res.status(404).json({ message: 'Servicio no encontrado.' });
+
+    // Actualizar campos opcionales
+    Object.keys(updateData).forEach((key) => {
+      (employee as any)[key] = updateData[key];
+    });
+
+    await employee.save();
+
+    res.status(200).json({ message: 'Servicio actualizado correctamente.', employee });
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al actualizar el empleado.', error: error.message });
+  }
+};
--- src/routes/services.routes.ts
+++ src/routes/services.routes.ts
@@ -1,14 +1,21 @@
 import { Router } from 'express'
 import { DeleteServiceController } from '../controllers/services/delete.controller';
-import ServicesValidateFieldsMiddleware from '../middleware/services/createServiceMiddleware';
-import { ObtainAllServicesController, CreateServiceController, ObtanTopServicesByEmployee } from '../controllers/services';
+import { validateFieldsCreateService } from '../middleware/services/createServiceMiddleware';
+import { validateFieldsUpdateService } from '../middleware/services/updateServiceMiddleware';
+import { 
+  ObtainAllServicesController, CreateServiceController, 
+  ObtanTopServicesByEmployee, ObtainServiceByIdController 
+} from '../controllers/services';
 
 const routerServices = Router()
 
 routerServices.get("/obtain/all", ObtainAllServicesController)
+routerServices.get("/obtain/:serviceId", ObtainServiceByIdController)
+
 routerServices.get("/top-services/:employeeId", ObtanTopServicesByEmployee)
 
-routerServices.post("/create", ServicesValidateFieldsMiddleware, CreateServiceController)
+routerServices.post("/create", validateFieldsCreateService, CreateServiceController)
 routerServices.delete("/delete/:serviceId", DeleteServiceController)
+routerServices.put("/update/:serviceId", validateFieldsUpdateService, DeleteServiceController)
 
 export default routerServices
