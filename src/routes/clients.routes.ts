74548012117420ae9e76bb504b898a2d22ec317b
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-24 20:16:27 -0300

    feat: add authentication middleware to client creation route

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { getClientController } from '../controllers/clients/obtain.controller';
 import { createClientController } from '../controllers/clients/create.controller';
 import validateFieldsInsertClient from '../middleware/clients/createClientMiddleware';
+import HandleAutentification from '../middleware/authentification';
 
 const routerClients = Router()
 
 routerClients.get("/obtain/all", getClientController)
-routerClients.post("/create", validateFieldsInsertClient, createClientController)
+routerClients.post("/create", HandleAutentification, validateFieldsInsertClient, createClientController)
 
 export default routerClients
