be13faa802642606a50c7550894699a462fc1c03
Enzo-taiel <enzonavarro28@gmail.com>
2025-10-24 20:14:49 -0300

    feat: update MongoDB connection message and add Redis connection handling

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -5,7 +5,7 @@ import { DATABASE_NSQL } from '../config/variables'
 export const connectDB = async () => {
   try {
     await mongoose.connect(DATABASE_NSQL.DATABASE_NSQL_URI + DATABASE_NSQL.DATABASE_NSQL_DB)
-    console.log("Connexion con Mongodb exitosa !")
+    console.log("[Mongodb] Conectado exitosamente ✅")
   } catch (error) {
     console.error('Error al conectar a MongoDB:', error);
     process.exit(1); // Finaliza el proceso si no puede conectarse
