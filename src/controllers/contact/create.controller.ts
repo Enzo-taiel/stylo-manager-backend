d1deb5618716f282ffdade4922d231c2cb616c6b
Enzo-taiel <enzonavarro28@gmail.com>
2025-11-17 15:50:03 -0300

    se refactorizo y se utiliza el next para caoturar los erres en el middleware

--- src/controllers/contact/create.controller.ts
+++ src/controllers/contact/create.controller.ts
@@ -1,19 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 import { ContactModel } from "../../database/models/index.model";
 
-export const CreateMessageContactController = async (req: Request, res: Response) => {
+export const CreateMessageContactController = async (req: Request, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array()[0], success: false, error: true, message: "error in key value" });
-  
+  if (!errors.isEmpty()) return next({ inputError: errors.array()[0] })
+
   const sessionId = req.sessionId
   try {
-    const message = await ContactModel.create({...req.body, sessionId })
+    const message = await ContactModel.create({ ...req.body, sessionId })
     // ANTES DE TERMINAR LA CONSULTA, AGREGAR LA LOGICA PARA RESPONDER EL MENSAJE MEDIANTE
     // WHATSAPP BUSSINES PARA PONERSE EN CONTACTO CON EL CLIENTE
     return res.status(200).json({ message: "message saved successfully.", success: true, error: false })
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ message: "Error internal Server.", success: false, error: true})
+    return next(error)
   }
 }
--- src/controllers/contact/obtain.controller.ts
+++ src/controllers/contact/obtain.controller.ts
@@ -1,23 +1,22 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { ContactModel } from "../../database/models/index.model";
 
-export const getAllMessages = async (_req: Request, res: Response) => {
+export const getAllMessages = async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const messages = await ContactModel.find({}, { phone: 1, full_name: 1, messages: { $slice: -1 } });
     return res.status(200).json({ message: "Messages obtain successfully.", messages, success: true, error: false })
   } catch (error) {
-    console.error(error)
-    return res.status(500).json({ message: "Error internal Server.", error: true, success: false })
+    return next(error)
   }
 }
 
-export const getAllMessageByPhone = async (req: Request, res: Response) => {
+export const getAllMessageByPhone = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { phone } = req.params;
     const messages = await ContactModel.findOne({ phone });
-    if (!messages) return res.status(404).json({ message: "Contacto no encontrado", error: true, success: false });
+    if (!messages) return next({ status: 404, message: "Contact not found."})
     return res.status(200).json({ messages, message: "Messages obtain successfully", success: true, error: false });
   } catch (error) {
-    return res.status(500).json({ message: "Error al obtener los mensajes", success: false, error: true });
+    return next(error)
   }
 }
