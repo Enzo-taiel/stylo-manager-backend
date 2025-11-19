0397405f8b41cc8f8b6aabdfe261f1663a1d9722
Enzo-taiel <enzonavarro28@gmail.com>
2025-11-19 01:11:22 -0300

    se agregó la ruta para obtener un negocio por ID, mejorando la funcionalidad del controlador

--- src/routes/business.routes.ts
+++ src/routes/business.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateBusinessController } from '../controllers/business/create.controller';
+import { ObtainBusinessByIdController } from '../controllers/business/obtain.controller';
+
 import HandleAutentification from '../middleware/authentification';
 import validateFieldsInsertBussine from '../middleware/business/createBusinessMiddleware';
 
 const routerBusiness = Router()
 
+routerBusiness.get("/obtain/:businessId", HandleAutentification, ObtainBusinessByIdController)
 routerBusiness.post("/create", HandleAutentification, validateFieldsInsertBussine, CreateBusinessController)
 
 export default routerBusiness
