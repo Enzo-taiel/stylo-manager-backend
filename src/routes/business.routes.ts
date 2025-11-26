d1ad9487e52073496930bc65bc38b1d4ac739a0b
Enzo-taiel <enzonavarro28@gmail.com>
2025-11-25 22:57:12 -0300

    se reorganizaron las rutas de negocio para mejorar la claridad, actualizando la ruta de obtención por subdominio y agrupando los controladores importados

--- src/routes/business.routes.ts
+++ src/routes/business.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
-import { CreateBusinessController } from '../controllers/business/create.controller';
-import { ObtainBusinessByIdController } from '../controllers/business/obtain.controller';
+import { CreateBusinessController, ObtainAllBusinessController, 
+  ObtainBusinessByIdController, ObtainBusinessBySubdomainController
+} from '../controllers/business/';
 
 import HandleAutentification from '../middleware/authentification';
 import validateFieldsInsertBussine from '../middleware/business/createBusinessMiddleware';
 
 const routerBusiness = Router()
 
-routerBusiness.get("/obtain/:businessId", HandleAutentification, ObtainBusinessByIdController)
+routerBusiness.get("/obtain/by-subdomain/:subdomain", HandleAutentification, ObtainBusinessByIdController)
+
+// routes used in expo app
+routerBusiness.get("/obtain/:businessId", HandleAutentification, ObtainBusinessBySubdomainController)
 routerBusiness.post("/create", HandleAutentification, validateFieldsInsertBussine, CreateBusinessController)
 
 export default routerBusiness
